97b15f69267093b2e91fab7bdadf624ff399e89f
Rafael <rafael.oliveira@sesidf.org.br>
2024-08-22 08:24:13 -0300

    atividade

---
+++ atv2.py
@@ -0,0 +1,11 @@
+dist = input('Digite uma distância em metros: ')
+
+dist = float(dist)
+
+cm1 = dist * 100
+km1 = dist / 1000
+mm1 = dist * 1000
+
+print(f'A distância em cm é: {cm1}')
+print(f'A distância em km é: {km1}')
+print(f'A distância em mm é: {mm1}')
